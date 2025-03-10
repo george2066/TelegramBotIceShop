ff256a169018e0acfc795814178f75a8da21e5bc
george2066 <george2066@yandex.ru>
2025-03-10 12:56:06 +0300

    add: admin panel in browser

--- app/infra/postgres/db.py
+++ app/infra/postgres/db.py
@@ -8,16 +8,16 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Database:
     def __init__(self, dsn: Secret[PostgresDsn], declarative_base: type[DeclarativeBase]):
-        self._engine = create_async_engine(str(dsn.get_secret_value()))
-        self._async_session = async_sessionmaker(self._engine)
+        self.engine = create_async_engine(str(dsn.get_secret_value()))
+        self._async_session = async_sessionmaker(self.engine)
 
         self._declarative_base = declarative_base
 
     async def shutdown(self) -> None:
-        await self._engine.dispose()
+        await self.engine.dispose()
 
     async def create_tables(self) -> None:
-        async with self._engine.begin() as conn:
+        async with self.engine.begin() as conn:
             await conn.run_sync(self._declarative_base.metadata.create_all)
 
     @asynccontextmanager
--- settings/config.py
+++ settings/config.py
@@ -7,8 +7,13 @@ from pydantic_settings import SettingsConfigDict, BaseSettings
 class AppSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(__file__), ".envs/.env"))
 
-    TELEGRAM_API_KEY: SecretStr = SecretStr('secret')
+    TELEGRAM_API_KEY: SecretStr = SecretStr("secret")
     LOG_LEVEL: str = "INFO"
+
     POSTGRES_DSN: Secret[PostgresDsn] = Secret(
-        PostgresDsn('postgresql+asyncpg://postgres:postgres@localhost:5432/postgres')
-    )
+        PostgresDsn("postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"))
+
+    ADMIN_INTERFACE_PORT: int = 8001
+    ADMIN_SECRET_KEY: SecretStr = SecretStr("secretkey")
+    ADMIN_LOGIN: SecretStr = SecretStr("admin")
+    ADMIN_PASSWORD: SecretStr = SecretStr("admin")
